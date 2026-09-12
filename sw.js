4da4f3c75a55bf8ea9fb38ea8ad8386a60a7ea1a
yw6bdt7v7t-arch <yw6bdt7v7t@privaterelay.appleid.com>
2026-09-12 09:00:24 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
-const CACHE="flappy-retro-v10";
-const FILES=["./","./index.html","./game.js","./manifest.webmanifest"];
+const CACHE="flappy-retro-v3";const FILES=["./","./index.html","./game.js","./manifest.webmanifest"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(v=>v!==CACHE).map(v=>caches.delete(v)))).then(()=>self.clients.claim())));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>e.respondWith(fetch(e.request).then(r=>{const q=r.clone();caches.open(CACHE).then(c=>c.put(e.request,q)).catch(()=>{});return r}).catch(()=>caches.match(e.request))));
