769ed340385fdf1025d04e4deac8a5ef7ae3e354
yw6bdt7v7t-arch <yw6bdt7v7t@privaterelay.appleid.com>
2026-09-12 08:45:58 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE="flappy-retro-v5";const FILES=["./","./index.html","./game.js","./manifest.webmanifest"];
+const CACHE="flappy-retro-v10";
+const FILES=["./","./index.html","./game.js","./manifest.webmanifest"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(a=>Promise.all(a.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener("fetch",e=>e.respondWith(fetch(e.request).then(r=>{const q=r.clone();caches.open(CACHE).then(c=>c.put(e.request,q));return r}).catch(()=>caches.match(e.request))));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(v=>v!==CACHE).map(v=>caches.delete(v)))).then(()=>self.clients.claim())));
+self.addEventListener("fetch",e=>e.respondWith(fetch(e.request).then(r=>{const q=r.clone();caches.open(CACHE).then(c=>c.put(e.request,q)).catch(()=>{});return r}).catch(()=>caches.match(e.request))));
